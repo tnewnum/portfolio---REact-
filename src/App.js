4d56ce18290f2660cc189a33c8ebe3572a9050dc
Thomas Newnum <thomas.newnum@gmail.com>
2023-04-21 10:46:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +22,79 @@ function App() {
   );
 }
 
-export default App;
+export function Nav() {
+  return (
+<nav
+  className="navbar navbar-expand-md bg-dark-subtle shadow-lg"
+  id="myHeader"
+>
+  <div className="container-xxl">
+    <a href="#" className="navbar-brand">
+      <span className="fw-bold text-secondary">Tommy Newnum - Developer</span>
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#main-nav"
+      aria-controls="main-nav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div
+      className="collapse navbar-collapse justify-content-end align-center"
+      id="main-nav"
+    >
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <i
+              className="fa-solid fa-house fa-lg"
+              style={{ color: "#000000" }}
+            />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#about" className="nav-link">
+            About Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#contact" className="nav-link">
+            Contact Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="./assets/docs/resume.pdf"
+            target="blank"
+            className="nav-link"
+          >
+            Resume
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#proficiencies" className="nav-link">
+            Proficiencies
+          </a>
+        </li>
+        <li className="nav-item d-md-none">
+          <a href="#projects" className="nav-link">
+            Projects
+          </a>
+        </li>
+        <li className="nav-item ms-2 d-none d-md-inline">
+          <a href="#projects" className="btn btn-secondary">
+            Projects
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+    );
+}
