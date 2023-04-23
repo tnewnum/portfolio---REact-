e8ca0fa0b057f6c16508139fd59a27b7a4b644b5
Thomas Newnum <thomas.newnum@gmail.com>
2023-04-23 12:09:14 -0400

    broke NAV out into its own folder

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useState, useRef } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faEnvelope, faPerson, faAt} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPerson, faAt} from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Nav from './components/Nav'
 
 
 
@@ -17,91 +18,6 @@ export default function App() {
     <Footer />
     </>
   );
-}
-
-function scrollIntoView(ref) {
-  if (ref.current) {
-    ref.current.scrollIntoView({ behavior: "smooth" });
-  }
-}
-
-function Nav() {
-  const aboutRef = useRef(null);
-  const projectsRef = useRef(null);
-  const contactRef = useRef(null);
-  const proficienciesRef = useRef(null);
-
-  
-  return (
-<nav
-  className="navbar navbar-expand-md bg-dark-subtle shadow-lg"
-  id="myHeader"
->
-  <div className="container-xxl">
-    <a href="/" className="navbar-brand">
-      <span className="fw-bold text-secondary">Tommy Newnum - Developer</span>
-    </a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#main-nav"
-      aria-controls="main-nav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div
-      className="collapse navbar-collapse justify-content-end align-center"
-      id="main-nav"
-    >
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a href="/" className="nav-link">
-            <FontAwesomeIcon icon={faHouse}/>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href='#about' onClick={() => scrollIntoView(aboutRef)} className='nav-link'>
-            About
-          </a>
-         </li>
-        <li className="nav-item">
-          <a href="#contact" onClick={() => scrollIntoView(contactRef)} className="nav-link">
-            Contact Me
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href={process.env.PUBLIC_URL + "/docs/resume.pdf"}
-            target="blank"
-            rel="noopener noreferrer"
-            className="nav-link"
-          >
-            Resume
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#proficiencies" onClick={() => scrollIntoView(proficienciesRef)} className="nav-link">
-            Proficiencies
-          </a>
-        </li>
-        <li className="nav-item d-md-none">
-          <a href="#projects"onClick={() => scrollIntoView(projectsRef)} className="nav-link">
-            Projects
-          </a>
-        </li>
-        <li className="nav-item ms-2 d-none d-md-inline">
-          <a href="#projects" onClick={() => scrollIntoView(projectsRef)}className="btn btn-secondary">
-            Projects
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    );
 }
 
 function About() {
